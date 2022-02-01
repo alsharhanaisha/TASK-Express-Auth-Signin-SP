e4b0fe587053d63198ef6321d482bda35b9b764c
yanalshmilan <yshmilan@gmail.com>
2022-02-02 00:51:58 +0200

    done

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const connectDb = require('./database');
+const productsRoutes = require('./api/products/routes');
+const shopsRoutes = require('./api/shops/routes');
+const userRoutes = require('./api/users/routes');
+const app = express();
+connectDb();
+
+app.use(cors());
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(
+    `${req.method} ${req.protocol}://${req.get('host')}${req.originalUrl}`
+  );
+  next();
+});
+
+// Routes
+app.use('/api/products', productsRoutes);
+app.use('/api/shops', shopsRoutes);
+app.use('/api', userRoutes);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({
+    message: err.message || 'Internal Server Error',
+  });
+});
+app.listen(process.env.PORT || 5000);
---
+++ models/User.js
@@ -0,0 +1,31 @@
+const { model, Schema } = require('mongoose');
+
+const userSchema = new Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: 'Email address is required',
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      'Please fill a valid email address',
+    ],
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+});
+module.exports = model('User', userSchema);
